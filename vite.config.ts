efbdefd1291abeed22867c6645e69866886fe28e
Yixia Zheng <3240103945@zju.edu.cn>
2026-02-02 22:17:34 +0800

    Set base path for the project in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 添加下面这一行，仓库名记得前后都要有斜杠
+      base: '/Memory-record/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
